82bfef51a41032f211b0d085b74da597fc3723f5
Fjalar <fjalar@fjalar.org>
2023-07-28 16:38:26 +1000

    Intermediate update.

--- unravel/cluster.py
+++ unravel/cluster.py
@@ -10,33 +10,36 @@ def histplot(data):
     plt.hist(data, bins=data.max())
     plt.show()
 
+def cull(data):
+    cols = list(data.columns)
+    tocull = []
+    # Remove relationship grid columns: 57
+    tocull.append([col for col in cols if 'urx' in col])
+    # 'Replicate weight' columns: 270
+    tocull.append([ col for col in cols
+                  if 'Replicate weight' in meta.column_names_to_labels[col] ])
+    tocull = set(tocull)
+    for col in tocull: cols.remove(col)
+    return data[cols]
+
 # HILDA data for ISCO33: 106 rows, all 4203 columns.
-h33 = hilda_by_isco(33)
-# Subtract mean, divide by standard deviation.
-h33scaled = StandardScaler().fit_transform(h33)
-hscaled = StandardScaler().fit_transform(hilda)
-# At this clustering depth: 1830 clusters, biggest is #3 with 705 features.
-clustering = FeatureAgglomeration( n_clusters=None
-                                 , distance_threshold=2).fit(h33)
-# This seems better. Maximum at #20 with 1819 features but better distribution.
-clustering = FeatureAgglomeration(n_clusters=100).fit(h33scaled)
-clustering = FeatureAgglomeration(n_clusters=100).fit(hscaled)
+# h33 = hilda_by_isco(33)
+# # Subtract mean, divide by standard deviation.
+# h33scaled = StandardScaler().fit_transform(h33)
+# hscaled = StandardScaler().fit_transform(hilda)
+# # At this clustering depth: 1830 clusters, biggest is #3 with 705 features.
+# clustering = FeatureAgglomeration( n_clusters=None
+                                 # , distance_threshold=2).fit(h33)
+# # This seems better. Maximum at #20 with 1819 features but better distribution.
+# clustering = FeatureAgglomeration(n_clusters=100).fit(h33scaled)
+# clustering = FeatureAgglomeration(n_clusters=100).fit(hscaled)
 
-clustered = clustering.fit_transform(hscaled)
-data = pd.DataFrame( clustered
-                   , columns=['C'+str(i) for i in range(clustered.shape[1])] )
-# Cluster histogram.
-np.unique(clustering.labels_, return_counts=True)
+# clustered = clustering.fit_transform(hscaled)
+# data = pd.DataFrame( clustered
+                   # , columns=['C'+str(i) for i in range(clustered.shape[1])] )
+# # Cluster histogram.
+# np.unique(clustering.labels_, return_counts=True)
 
-# def cols_in_cluster(clusterindex):
-    # return [ meta.column_names_to_labels[col]
-             # for col in h33.columns[np.where( clustering.labels_ == clusterindex
-                                            # , True
-                                            # , False )] ]
-# def vars_in_cluster(index):
-    # return [ col for col in h33.columns[np.where( clustering.labels_ == index
-                                                # , True
-                                                # , False )] ]
 
 def cols_in_cluster( data # Data set (pandas dataframe)
                    , clustering # Clustering object.
@@ -75,22 +78,23 @@ def clusterkeywords(index, n=10, returndict=False):
 
 
 if __name__ == "__main__":
+    pass
     # Job satisfaction and its effects.
-    'ujbmsall' in vars_in_cluster(27)
-    g = discover(['PC', 'GES', 'CCDr'], data.sample(2000))
-    [ vertex for vertex in g.neighbors('C27') ] # -> C35
-    [ vertex for vertex in g.neighbors('C35') ] # -> C93
-    vertices = []
-    vertices += vars_in_cluster(27)
-    vertices += vars_in_cluster(35)
-    vertices += vars_in_cluster(93)
+    # 'ujbmsall' in vars_in_cluster(27)
+    # g = discover(['PC', 'GES', 'CCDr'], data.sample(2000))
+    # [ vertex for vertex in g.neighbors('C27') ] # -> C35
+    # [ vertex for vertex in g.neighbors('C35') ] # -> C93
+    # vertices = []
+    # vertices += vars_in_cluster(27)
+    # vertices += vars_in_cluster(35)
+    # vertices += vars_in_cluster(93)
 
-    # List of list of variables.
-    vertexlistoflists = [ vars_in_cluster(int(cluster[1:]))
-                          for cluster in subgraph(g, 'C27', depth=4).nodes() ]
-    vertices = [ vertex for sublist in vertexlistoflists for vertex in sublist ]
+    # # List of list of variables.
+    # vertexlistoflists = [ vars_in_cluster(int(cluster[1:]))
+                          # for cluster in subgraph(g, 'C27', depth=4).nodes() ]
+    # vertices = [ vertex for sublist in vertexlistoflists for vertex in sublist ]
 
 
-    h = discover(['PC', 'GES', 'CCDr'], hilda[vertices].sample(1000))
-    [ meta.column_names_to_labels[var]
-      for var in markov_blanket(h, 'ujbmsall').nodes() ]
+    # h = discover(['PC', 'GES', 'CCDr'], hilda[vertices].sample(1000))
+    # [ meta.column_names_to_labels[var]
+      # for var in markov_blanket(h, 'ujbmsall').nodes() ]
