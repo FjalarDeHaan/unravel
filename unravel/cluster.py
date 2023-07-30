4e480c9bb450854c8f6b3c694a9afeaf485424ea
Fjalar <fjalar@fjalar.org>
2023-07-30 16:38:35 +1000

    Added recursive OPTICS clustering.

--- unravel/cluster.py
+++ unravel/cluster.py
@@ -3,43 +3,72 @@ from unravel import *
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import FeatureAgglomeration
 from sklearn.cluster import DBSCAN
+from sklearn.cluster import OPTICS
 import matplotlib.pyplot as plt
 import stopwords
+import difflab
 
 def histplot(data):
     plt.hist(data, bins=data.max())
     plt.show()
 
+def dis(s1, s2):
+    return 1 - difflib.SequenceMatcher( None
+                                      , s1.split()
+                                      , s2.split() ).ratio()
+
+
 def cull(data):
-    cols = list(data.columns)
-    tocull = []
+    cols = data.columns.to_list()
     # Remove relationship grid columns: 57
-    tocull.append([col for col in cols if 'urx' in col])
+    cs1 = {col for col in cols if 'urx' in col}
     # 'Replicate weight' columns: 270
-    tocull.append([ col for col in cols
-                  if 'Replicate weight' in meta.column_names_to_labels[col] ])
-    tocull = set(tocull)
+    cs2 = { col for col in cols
+                if 'Replicate weight' in meta.column_names_to_labels[col] }
+    # 'Imputation flag' columns: 125
+    cs3 = { col for col in cols
+                if 'Imputation flag' in meta.column_names_to_labels[col] }
+    # 'Population weight' columns: 8
+    cs4 = { col for col in cols
+                if 'Population weight' in meta.column_names_to_labels[col] }
+    # 'interview outcome' columns: 30
+    cs5 = { col for col in cols
+                if 'interview outcome' in meta.column_names_to_labels[col] }
+    # '(relationship)' columns: 45
+    cs6 = { col for col in cols
+                if '(relationship)' in meta.column_names_to_labels[col] }
+    # 'Relationship in household' columns: 10
+    cs7 = { col for col in cols
+                if 'Relationship in household'
+                in meta.column_names_to_labels[col] }
+    # 'Income unit' columns: 11
+    cs8 = { col for col in cols
+                if 'Income unit' in meta.column_names_to_labels[col] }
+    # 'Family type' columns: 10
+    cs9 = { col for col in cols
+                if 'Family type' in meta.column_names_to_labels[col] }
+    # 'Family number person' columns: 11
+    cs10 = { col for col in cols
+                if 'Family number person' in meta.column_names_to_labels[col] }
+    # 'Relationship of self' columns: 13
+    cs11 = { col for col in cols
+                 if 'Relationship of self' in meta.column_names_to_labels[col] }
+    # 'Enumerated person' columns: 4
+    cs12 = { col for col in cols
+                 if 'Enumerated person' in meta.column_names_to_labels[col] }
+    # 'Imputed age' columns: 8
+    cs13 = { col for col in cols
+                 if 'Imputed age' in meta.column_names_to_labels[col] }
+    # 'Wave last interviewed' columns: 8
+    cs14 = { col for col in cols
+                 if 'Wave last interviewed' in meta.column_names_to_labels[col] }
+
+    tocull = set.union( cs1, cs2, cs3, cs4, cs5
+                      , cs6, cs7, cs8, cs9, cs10
+                      , cs11, cs12, cs13, cs14
+                      )
     for col in tocull: cols.remove(col)
-    return data[cols]
-
-# HILDA data for ISCO33: 106 rows, all 4203 columns.
-# h33 = hilda_by_isco(33)
-# # Subtract mean, divide by standard deviation.
-# h33scaled = StandardScaler().fit_transform(h33)
-# hscaled = StandardScaler().fit_transform(hilda)
-# # At this clustering depth: 1830 clusters, biggest is #3 with 705 features.
-# clustering = FeatureAgglomeration( n_clusters=None
-                                 # , distance_threshold=2).fit(h33)
-# # This seems better. Maximum at #20 with 1819 features but better distribution.
-# clustering = FeatureAgglomeration(n_clusters=100).fit(h33scaled)
-# clustering = FeatureAgglomeration(n_clusters=100).fit(hscaled)
-
-# clustered = clustering.fit_transform(hscaled)
-# data = pd.DataFrame( clustered
-                   # , columns=['C'+str(i) for i in range(clustered.shape[1])] )
-# # Cluster histogram.
-# np.unique(clustering.labels_, return_counts=True)
-
+    return data[cols], len(tocull)
 
 def cols_in_cluster( data # Data set (pandas dataframe)
                    , clustering # Clustering object.
@@ -56,6 +85,19 @@ def vars_in_cluster( data # Data set (pandas dataframe)
     return [ col for col in data.columns[np.where( clustering.labels_ == index
                                                  , True
                                                  , False )] ]
+
+def pad(s1, s2):
+    """Pad shortest string with spaces, return adjusted pair."""
+    # Equal length strings. Do nothing.
+    if len(s1) == len(s2): return s1, s2
+    # String `s1` shorter. Pad it.
+    elif len(s1) < len(s2):
+        d = len(s2) - len(s1)
+        return s1 + d*' ', s2
+    # String `s2` shorter. Pad it.
+    elif len(s1) > len(s2):
+        d = len(s1) - len(s2)
+        return s1, s2 + d*' '
 
 def text_in_cluster(data, clustering, index):
     words = ' '.join(cols_in_cluster(data, clustering, index)).split()
@@ -76,25 +118,36 @@ def keywords(text, n=10, returndict=False):
 def clusterkeywords(index, n=10, returndict=False):
     return keywords(text_in_cluster(index), n=n, returndict=returndict)
 
+def dmatrix(labels):
+    A = np.zeros((len(labels), len(labels)))   
+    for i in range(len(labels)):
+        for j in range(1+i, len(labels)):
+            A[i, j] = A[j, i] = dis(labels[i], labels[j])
+    return A
 
 if __name__ == "__main__":
-    pass
-    # Job satisfaction and its effects.
-    # 'ujbmsall' in vars_in_cluster(27)
-    # g = discover(['PC', 'GES', 'CCDr'], data.sample(2000))
-    # [ vertex for vertex in g.neighbors('C27') ] # -> C35
-    # [ vertex for vertex in g.neighbors('C35') ] # -> C93
-    # vertices = []
-    # vertices += vars_in_cluster(27)
-    # vertices += vars_in_cluster(35)
-    # vertices += vars_in_cluster(93)
+    h, _ = cull(hilda)
+    h33, _ = cull(hilda_by_isco(33))
+    labels = np.array( [ meta.column_names_to_labels[col]
+                         for col in h.columns ] )
+    A = dmatrix(labels)
+    clustering = OPTICS(metric='precomputed').fit(A)
 
-    # # List of list of variables.
-    # vertexlistoflists = [ vars_in_cluster(int(cluster[1:]))
-                          # for cluster in subgraph(g, 'C27', depth=4).nodes() ]
-    # vertices = [ vertex for sublist in vertexlistoflists for vertex in sublist ]
+    for i in range(10):
+        B = dmatrix(labels[clustering.labels_ == -1])
+        clusteringB = OPTICS(metric='precomputed').fit(B)
+        mask = np.where( clusteringB.labels_ == -1
+                       , -1
+                       , ( clusteringB.labels_
+                         + clustering.labels_.max() )
+                       ) 
+        clustering.labels_[clustering.labels_ == -1] = mask 
+
+    
+    
 
 
-    # h = discover(['PC', 'GES', 'CCDr'], hilda[vertices].sample(1000))
-    # [ meta.column_names_to_labels[var]
-      # for var in markov_blanket(h, 'ujbmsall').nodes() ]
+
+
+
+
